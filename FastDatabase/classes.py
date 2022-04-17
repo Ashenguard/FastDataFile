db41120edee5b72638fc206a23875dc89f0e67a0
Ashenguard <myminecraft2019@gmail.com>
2022-04-17 07:54:10 +0430

    FastDatabase Update #3 - Critical bug!

--- FastDatabase/classes.py
+++ FastDatabase/classes.py
@@ -1,10 +1,20 @@
+from .databases import BaseDatabase, OnCloseDatabase, OnChangeDatabase, ManualDatabase, ThreadSafeDatabase
 from .encoders import DatabaseEncoder
 from .methods import DataMethod
 
 
-class Database:
-    def __init__(self, file_path: str, encoder: DatabaseEncoder, create_if_missing: bool = True, default_data=None, encoding='utf8', method: DataMethod = DataMethod.OnClose):
-        pass
+class Database(BaseDatabase):
+    def __new__(cls, file_path: str, encoder: DatabaseEncoder, create_if_missing: bool = True, default_data=None, encoding='utf8', method: DataMethod = DataMethod.OnClose):
+        if method == DataMethod.OnClose:
+            return OnCloseDatabase(file_path, encoder, create_if_missing, default_data, encoding)
+        if method == DataMethod.OnChange:
+            return OnChangeDatabase(file_path, encoder, create_if_missing, default_data, encoding)
+        if method == DataMethod.Manual:
+            return ManualDatabase(file_path, encoder, create_if_missing, default_data, encoding)
+        if method == DataMethod.ThreadSafe:
+            return ThreadSafeDatabase(file_path, encoder, create_if_missing, default_data, encoding)
+
+        raise TypeError(f'provided method is invalid')
 
 
 class JSONDatabase(Database):
