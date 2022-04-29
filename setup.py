f0d382375922aa113dc452061097aef7c263cd19
Ashenguard <myminecraft2019@gmail.com>
2022-04-29 19:21:55 +0430

    2.0 - Updating version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="FastDataFile",
-    version="1.0.2",
+    version="2.0.0",
     license='MIT License',
     author="Ashenguard",
     author_email="Ashenguard@agmdev.com",
